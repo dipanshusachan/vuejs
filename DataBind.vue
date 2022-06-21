08c64c5c0f0c03a957d59ead0b233638342c33ad
dipanshusachan <107169665+dipanshusachan@users.noreply.github.com>
2022-06-21 15:10:45 +0530

    Create DataBind.vue

---
+++ DataBind.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Binding the Data</h1>
+        <h1>{{message}}</h1>
+        <input type="text" v-model="message">
+
+        <h2 style="white-space:pre-line">{{message}}</h2>
+        <textarea name="text"  v-model="message"></textarea>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'DataBind',
+    data(){
+        return{
+            message:'Hello'
+        }
+    }
+}
+</script>
