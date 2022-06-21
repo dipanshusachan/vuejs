d54a2dce0a876ce8895e11314e740bd65a5405bd
dipanshusachan <107169665+dipanshusachan@users.noreply.github.com>
2022-06-21 15:11:57 +0530

    Create CompChild.vue

---
+++ CompChild.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="first">
+        <h1>Child component of Compmain</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Compmain'
+}
+</script>
+
+<style>
+    .first{
+        
+    background-color: #edbe81;
+    height: 100px;
+    width: 500px;
+    padding: 40px;
+}
+
+   
+</style>
