abc90fe338d531f6c028f20327bedadbcaee5c76
dipanshusachan <107169665+dipanshusachan@users.noreply.github.com>
2022-06-21 15:11:18 +0530

    Create CompMain.vue

---
+++ CompMain.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1><u>This is Main File</u></h1>
+        <Compchild/>
+        <Products/>
+    </div>
+</template>
+
+<script>
+import Compchild from './Compchild'
+import Products from './Products'
+export default {
+    name:'Compmain',
+    components:{
+        Compchild,
+        Products,
+    }
+}
+</script>
+
+<style>
+    div{
+            background-color: #cedae5;
+            height: 500px;
+            margin: 20px;
+}
+   
+</style>
